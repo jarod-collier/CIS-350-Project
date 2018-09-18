d67fef826daad18cc5a019c62b7e0a9cae31436c
shinjimasuka <collierj@mail.gvsu.edu>
2018-09-18 11:48:06 -0400

    First classes created Here we are just trying to get everyone onto Git and get all of the classes set up

---
+++ specialCardGames/GameLogic.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package specialCardGames;
+
+/**
+ * @author jarod
+ *
+ */
+public class GameLogic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
