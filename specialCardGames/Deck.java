4a7c9f7fab2ef2fc382ec1c1e4c971ef18ad2c02
shinjimasuka <collierj@mail.gvsu.edu>
2018-10-03 20:38:49 -0400

    First part of game logic is almost there Cards can now be chosen and the deck will do so up until the last card. lower or higher and inside or outside methods have been created to support the logic for the game.

--- specialCardGames/Deck.java
+++ specialCardGames/Deck.java
@@ -1,6 +1,7 @@
 package specialCardGames;
 
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.*;
 
 /**********************************************************************
@@ -22,30 +23,42 @@ import java.util.*;
 
 public class Deck {
 
+	/** 2D array used to hold all card values */
 	private int[][] deck;
 
+	/** List of possible card values to be shuffled */
 	private ArrayList<Integer> shuffled;
-	
+
+	/** ArraylIST of cards that have been chosen */
+	private ArrayList<Integer> chosenCards;
+
+	/** Random number generator for the suit */
 	private Random randomSuitGen;
-	
+
+	/** Random number generator for the value */
 	private Random randomValueGen;
-	
+
+	/** int used to keep track of the suit being picked */
 	private int randomSuit;
-	
+
+	/** int used to keep track of the value being picked */
 	private int randomValue;
 
-
+	/** final variable used for the number of suits in a deck */
 	private final int SUITS = 4;
 
+	/** final variable used for the number of values in a deck */
 	private final int VALUES = 13;
 
 	public static void main(String[] args) {
 		Deck d = new Deck();
 		d.printDeck();
-		System.out.println(d.chooseSuit());
-		System.out.println(d.blackOrRed(d.chooseSuit()));
-		System.out.println(d.chooseValue());
 
+		for (int i = 0; i < 52; i++) {
+			System.out.println("\nThe chosen card has a value of: " + d.chooseCard() + "\n");
+			d.printDeck();
+		}
+		System.out.println(d.lowerOrHigher());
 	}
 
 
@@ -55,8 +68,10 @@ public class Deck {
 	 *****************************************************************/
 	public Deck() {
 
+		// Instantiates lists and arrays
 		deck = new int[VALUES][SUITS];
 		shuffled = new ArrayList<Integer>();
+		chosenCards = new ArrayList<Integer>();
 
 		// Add values into the deck
 		for (int value = 0; value < VALUES; value++) {
@@ -68,16 +83,53 @@ public class Deck {
 		// Add possible values to be shuffled
 		for (int shuffle = 0; shuffle < VALUES; shuffle++)
 			shuffled.add(shuffle + 2);
-		
-		
+
+
 		shuffle(); 
-		
+
+		// Instantiates random number generators
 		randomSuitGen = new Random();
 		randomValueGen = new Random();
-		
+
+		// Instantiates random suit and value variables
 		randomSuit = randomSuitGen.nextInt(SUITS);
 		randomValue = randomValueGen.nextInt(VALUES) + 2;
 	} 
+
+	/******************************************************************
+	 * Gets the suit value that is chosen at random
+	 * @return the randomSuit value
+	 *****************************************************************/
+	public int getRandomSuit() {
+		return randomSuit;
+	}
+
+
+	/******************************************************************
+	 * Sets the suit to be something the user chooses
+	 * @param randomSuit - the randomSuit to set
+	 *****************************************************************/
+	public void setRandomSuit(int randomSuit) {
+		this.randomSuit = randomSuit;
+	}
+
+
+	/******************************************************************
+	 * Gets the value of the card that is chosen at random
+	 * @return the randomValue of the card
+	 *****************************************************************/
+	public int getRandomValue() {
+		return randomValue;
+	}
+
+
+	/******************************************************************
+	 * Sets the value of a card to whatever the user chooses
+	 * @param randomValue - the randomValue to set
+	 *****************************************************************/
+	public void setRandomValue(int randomValue) {
+		this.randomValue = randomValue;
+	}
 
 
 	/******************************************************************
@@ -85,56 +137,66 @@ public class Deck {
 	 *****************************************************************/
 	public void shuffle() {
 
+		// Shuffles the ArrayList of possible values
 		Collections.shuffle(shuffled);
 
+		// Takes the shuffled arrayList and adds the values to the deck
 		for (int suit = 0; suit < SUITS; suit++) {	
 			for (int value = 0; value < VALUES; value++) {
-					deck[value][suit] = shuffled.get(value);
+				deck[value][suit] = shuffled.get(value);
 			}
+
+			// Shuffles the arrayList so each suit is shuffled
 			Collections.shuffle(shuffled);
 		}
 	}
-	
+
 	/******************************************************************
 	 * Randomly chooses a value from the deck
 	 * @return int value 2-14 that represents the value of each card.
 	 * 11 is Jack, 12 is Queen, 13 is King, and 14 is Ace. 
 	 *****************************************************************/
 	public int chooseValue() {
-		
+
+		// Resets the random value
+		randomValue = randomValueGen.nextInt(VALUES) + 2;
+
 		// Makes sure that there is a playable value in the suit
 		int count = 0;
 		for (int suit = 0; suit < SUITS; suit++) {
-			if (deck[randomValue][suit] == 0) 
+			if (deck[randomValue - 2][suit] == 0) 
 				count++;	
 		}
-		
+
 		// If no playable cards, randomly pick new value and try again
 		if (count == SUITS) {
-			randomValue = randomValueGen.nextInt(VALUES);
+			randomValue = randomValueGen.nextInt(VALUES) + 2;
 			return chooseValue();
 		}
 		else {
 			return randomValue;
 		}
-		
+
 	}
-	
-	
+
+
 	/******************************************************************
 	 * Randomly chooses a suit from the deck
 	 * @return int value 0-3 that represents the suit. 0 is Spades, 1
 	 * is Hearts, 2 is Clubs, and 3 is Diamonds
 	 *****************************************************************/
 	public int chooseSuit() {
-		
+
+		// Resets the random suit value
+		randomSuit = randomSuitGen.nextInt(SUITS);
+
 		// Makes sure that there is a playable card in the suit
 		int count = 0;
 		for (int val = 0; val < VALUES; val++) {
 			if (deck[val][randomSuit] == 0) 
 				count++;	
 		}
-		
+
 		// If no playable cards, randomly pick new suit and try again
 		if (count == VALUES) {
 			randomSuit = randomSuitGen.nextInt(SUITS);
@@ -143,23 +205,115 @@ public class Deck {
 		else {
 			return randomSuit;
 		}
-		
+
 	}
-	
+
+	/******************************************************************
+	 * Takes a random value and random suit and actually chooses to 
+	 * place or use that card. Once the card is used it is removed.
+	 * @param chooseValue - int used for the value in the deck
+	 * @param chooseSuit - int used for the suit in the deck
+	 * @return 2D int array representing the chosen cards
+	 * @throws IllegalArgumentException when the deck is empty but a 
+	 * card is attempted to be chosen
+	 *****************************************************************/
+	public int chooseCard(){
+
+		// Checks if the deck is empty
+		if (emptyDeck())
+			throw new IllegalArgumentException("Deck is empty");
+
+		// Get a random value for the suit and value
+		chooseSuit();
+		chooseValue();
+
+		if (checkUsedCard(randomValue - 2, randomSuit))
+			return chooseCard();
+
+		// Adds the choice to a list of chosen cards
+		chosenCards.add(deck[randomValue - 2][randomSuit]);
+
+		// Removes the chosen card from the deck
+		usedCard(randomValue - 2, randomSuit);
+
+		return chosenCards.get(chosenCards.size() -1); 
+
+	}
+
 	/******************************************************************
 	 * Checks whether the suit picked is black or red.
 	 * @param suit - int from 0-3 that represents the suit chosen
 	 * @return int value of 0 or 1. 0 if black, 1 if red.
 	 *****************************************************************/
 	public int blackOrRed(int suit) {
+
+		// If the card is a spade or club, it's black, so return 0
 		if (chooseSuit() == 0 || chooseSuit() == 2)
 			return 0;
 		else
 			return 1;
 	}
-	
-	
-	
+
+	/******************************************************************
+	 * Checks whether new card is higher, lower, or equal to the 
+	 * previous card
+	 * @return -1, 0, or 1 depending on if lower, equal, or higher
+	 * @throws IllegalArgumentException when not enough cards have been
+	 * picked
+	 *****************************************************************/
+	public int lowerOrHigher() {
+
+		// Checks if at least 2 cards have been chosen to compare
+		if (chosenCards.size() > 1) {
+
+			// If recent card is bigger, return 1
+			if (chosenCards.get(chosenCards.size() - 1) >
+			(chosenCards.get(chosenCards.size() - 2)))
+				return 1;
+
+			// If recent card is smaller, return -1
+			else if (chosenCards.get(chosenCards.size() - 1) <
+					(chosenCards.get(chosenCards.size() - 2)))
+				return -1;
+
+			// If cards are the same, return 0
+			else
+				return 0;	
+		}
+		else
+			throw new IllegalArgumentException("Can't compare cards");
+
+	}
+
+	/******************************************************************
+	 * Determines whether the next card is inside, outside, or equal
+	 * to the previous 2 cards
+	 * @return -1, 0, or 1 whether the card is inside, equal,
+	 * or outside
+	 * @throws IllegalArgumentException when you can't compare cards
+	 *****************************************************************/
+	public int insideOrOutside() {
+
+		int first = chosenCards.get(chosenCards.size() - 3);
+		int second = chosenCards.get(chosenCards.size() - 2);
+		int third = chosenCards.get(chosenCards.size() - 1);
+
+		// Checks if at least 3 cards have been chosen to compare
+		if (chosenCards.size() > 2) {
+
+			if (third > first && third < second)
+				return -1;
+			else if ( (third > first && third > second) ||
+					(third < first && third < second))
+				return 1;
+			else
+				return 0;
+		}
+		else
+			throw new IllegalArgumentException("Can't compare cards");
+
+	}
+
 	/******************************************************************
 	 * Sets a card to be 0
 	 * @param row - the row of the deck array 
@@ -168,7 +322,7 @@ public class Deck {
 	public void usedCard(int value, int suit) {
 		deck[value][suit] = 0;	
 	}
-	
+
 	/******************************************************************
 	 * Checks if the card has been used or not. If it has been used
 	 * then it will be 0 and then this method will return true
@@ -181,6 +335,29 @@ public class Deck {
 			return true;
 		else
 			return false;
+	}
+
+	/******************************************************************
+	 * Checks whether the deck is empty
+	 * @return true or false whether the deck is empty
+	 *****************************************************************/
+	public boolean emptyDeck() {
+
+		// Checks whether the deck has cards in it
+		int count = 0;
+		for (int value = 0; value < VALUES; value++) {
+			for (int suit = 0; suit < SUITS; suit++) {	
+				if (checkUsedCard(value, suit))
+					count++;		
+			}
+		}
+
+		// If there are no available cards, return return
+		if (count == SUITS * VALUES)
+			return true;
+		else
+			return false;
+
 	}
 
 
