d31d9728debd5044999e40d6d3105afa7cf5fd48
Mazen Ashgar <Mazen.ashgar@gmail.com>
2018-10-05 13:35:03 -0400

    Created the window and added the wallpaper for the game.

--- specialCardGames/GamePanel.java
+++ specialCardGames/GamePanel.java
@@ -1,4 +1,41 @@
 package specialCardGames;
 
-public class GamePanel {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+
+public class GamePanel extends JPanel {
+    private Image backGround = null;
+
+    public GamePanel()
+    {
+        JPanel superPanel = new JPanel();
+        superPanel.setLayout(new GridLayout());
+        add(superPanel, "Center");
+
+        changeImage();
+        setPreferredSize(new Dimension(1020, 570));
+    }
+
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.drawImage(this.backGround, 0, 0, null);
+    }
+
+    public void changeImage()
+    {
+        try
+        {
+            this.backGround = ImageIO.read(getClass().getResource("bG.jpg"));
+            repaint();
+        }
+        catch (IOException e)
+        {
+            JOptionPane.showMessageDialog(this, "Couldn't load background");
+        }
+    }
 }
